7333295d08d08a505ccefd6ea62581ad9cce16ab
Noah Sperling <132321104+NoahAdamSperling@users.noreply.github.com>
2023-05-01 23:51:12 -0400

    Add files via upload

---
+++ Stock Orb, Crypto Market Version - Shareable.py
@@ -0,0 +1,96 @@
+# Import needed Python packages
+import requests
+import json
+import time
+from yeelight import Bulb
+
+# Enter your Yeelight's IP address here
+BULB_IP = "Your YeeLight's IP address"
+
+# Enter your API key for Finnhub here
+FINNHUB_API_KEY = "Your Finnnubb API KEY"
+
+# Connect to your Yeelight bulb
+bulb = Bulb(BULB_IP)
+bulb.turn_on()
+
+# Define a function to change the bulb color based on the percent change in Bitcoin price
+def change_color(percent_change):
+    if percent_change >= 3:
+        bulb.set_rgb(255, 255, 0)
+        # Yellow color for percent changes greater than or equal to 3
+    elif percent_change > 0:
+        bulb.set_rgb(0, 255, 0)
+        # Green color for percent changes between 0 and 3
+    elif percent_change <= -3:
+        bulb.set_rgb(128, 0, 128)
+        # Purple color for percent changes less than or equal to -3
+    elif percent_change < 0:
+         bulb.set_rgb(255, 0, 0)
+        # Red color for percent changes less than or equal to -3
+    else:
+         bulb.set_rgb(255, 255, 255)
+
+
+# Define a function to get the current Bitcoin price from Binance using the Finnhub API
+# 'resolution=1&count=1' tells:
+#     resolution=1 specifies the resolution of the data to be retrieved to be 1 second intervals.
+#     count=1 specifies that only one candle or data point is required, which in this case is the most 
+#             recent candle or data point, corresponding to the current Bitcoin price.
+def get_btc_price():
+    url = "https://finnhub.io/api/v1/crypto/candle?symbol=BINANCE:BTCUSDT&resolution=1&count=1&token=" + FINNHUB_API_KEY
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return data["c"][-1]
+
+
+# Define a function to get the previous Bitcoin price from Binance using the Finnhub API
+    # Get the timestamp for yesterday at 5:00pm
+    # 'resolution=60&count=1' tells:
+    #     resolution=1 specifies the resolution of the data to be retrieved to be 1 second intervals.
+    #     The 'from' and 'to' parameters in the URL specify the time range for which to retrieve the candle data, 
+    #     in Unix timestamp format. yesterday_5pm is a variable that stores the Unix timestamp for yesterday's date at 5:00 PM 
+    #     in the local timezone. yesterday_5pm+60*60 adds 60 minutes to the yesterday_5pm timestamp, which represents the end 
+    #     of the 1-hour interval for which the candle data is being retrieved.
+    # So the full URL with parameters is retrieving the candle data for Bitcoin's price from Binance for a 
+    # 1-hour interval that starts at yesterday's date at 5:00 PM and ends 1 hour later.
+def get_prev_btc_price():
+    yesterday_5pm = int(time.time()) - ((60 * 60 * 24) + (60 * 60 * 7))
+    url = f"https://finnhub.io/api/v1/crypto/candle?symbol=BINANCE:BTCUSDT&resolution=60&from={yesterday_5pm}&to={yesterday_5pm+60*60}&token={FINNHUB_API_KEY}"
+    response = requests.get(url)
+    data = json.loads(response.text)
+    return data["c"][-1]
+
+# Define a function to get the percent change in Bitcoin price
+def get_percent_change(current_price, prev_price):
+    return ((current_price - prev_price) / prev_price) * 100
+
+# Define a function to get the current Bitcoin price every 5 seconds and change the bulb color based on the percent change
+def run():
+    prev_price = get_prev_btc_price()
+    while True:
+        try:
+            current_price = get_btc_price()
+            percent_change = get_percent_change(current_price, prev_price)
+            change_color(percent_change)
+            now = time.strftime("%Y-%m-%d %H:%M:%S")
+            #^Add a time variable so the current time of each pull can be displayed in the terminal
+            print(f"{now} - Current Price: {current_price:.2f}, Previous Price: {prev_price:.2f}")
+            prev_price = get_prev_btc_price()
+            time.sleep(3)
+        #If an error occurs, print that an error occured to the terminal, then turn blue for 5 seconds, then restart
+        except Exception as e:
+            print(f"Error: {e}")
+            for i in range(30, 0, -1):
+                bulb.set_rgb(0, 0, 255)
+                time.sleep(5)
+                print(f"Restarting in {i} seconds...")
+            bulb.set_rgb(255, 255, 255)
+            time.sleep(5)
+
+# This Python code defines a conditional block that checks whether the current module is
+# being run as the main program. If the current module is being run as the main program,
+# then the function run() is called.
+# This is useful for writing test code and for defining a command-line interface for a module or package
+if __name__ == '__main__':
+    run()
---
+++ Stock Orb, Stock Market Version - Shareable.py
@@ -0,0 +1,128 @@
+# Import needed Python packages
+import datetime
+import pytz
+import time
+import requests
+import requests.adapters
+from yeelight import Bulb
+from finnhub import Client
+
+# Request adapters setting to 100 should resolve a pooling issue, set higher if crashing.
+requests.adapters.DEFAULT_POOL_SIZE = 100
+
+# Replace with your Finnhub API key
+API_KEY = "Your Finnnubb API KEY"
+
+# Replace with your Yeelight Smart Light's IP address
+# This IP address is found in the Yee Light app once the bulb is connected.
+BULB_IP = "Your YeeLight's IP address"
+
+# Replace with your target stock symbol
+# VOO is an applicable S&P 500 ETF
+SYMBOL = "VOO"
+
+# Replace with your target stock exchange
+EXCHANGE = "US"
+
+# Replace with your timezone
+TIMEZONE = "US/Eastern"
+
+while True:
+    #Try statement tells the code to re-run the code starting from this point downward if an error is encountered
+    try:
+        # Create a new instance of the Yeelight Smart Light
+        bulb = Bulb(BULB_IP)
+        
+        # Create a new instance of the Finnhub API client
+        finnhub_client = Client(api_key=API_KEY)
+        
+        # Initialize the previous closing stock price
+        previous_closing_price = None
+
+        while True:
+            # Defining current_time using the date.time function from online
+            current_time = datetime.datetime.now(pytz.timezone(TIMEZONE))
+            # If current weekday is under 5 (i.e., 0 Monday through 4 Friday) and more than 8:30am and less than 6:30pm
+            if current_time.weekday() < 5 and datetime.time(8, 30) <= current_time.time() <= datetime.time(18, 30):
+                
+                # Turn on the Yeelight Smart Light and set brightness to 50%
+                bulb.turn_on()
+                bulb.set_brightness(50)
+                
+                #Try-Except to Catch FinnHub API error
+                #Turn bulb blue for 30 seconds if this error occurs
+                try:
+                    # Connecting to the FinnHub API for the specific stock you specify
+                    stock_data = finnhub_client.quote(SYMBOL)
+                except Exception as e:
+                    print("Error accessing Finnhub API:", e)
+                    bulb.set_rgb(0, 0, 255)
+                    time.sleep(30)
+                    continue
+                
+                # Get the current stock price and previous day closing price
+                current_price = stock_data['c']
+                current_closing_price = stock_data['pc']
+
+                # If this is not the first iteration of the loop
+                if previous_closing_price is not None:
+                    # Calculate the percent change in stock price
+                    percent_change = (current_price - previous_closing_price) / previous_closing_price * 100
+                    # Change the color of the Yeelight Smart Light based on the percent change in stock price
+                    if percent_change >= 3:
+                        bulb.set_rgb(255, 215, 0)
+                    # Gold color for percent changes greater than or equal to 3
+                    elif percent_change > 0:
+                        bulb.set_rgb(0, 255, 0)
+                    # Green color for percent changes between 0 and 3
+                    elif percent_change <= -3:
+                        bulb.set_rgb(128, 0, 128)
+                     # Purple color for percent changes less than or equal to -3
+                    elif percent_change < 0:
+                        bulb.set_rgb(255, 0, 0)
+                    # Red color for percent changes less than or equal to -3
+                    else:
+                        bulb.set_rgb(255, 255, 255)
+                    # White color for everything else, generally will apply when change is zero from market being closed
+                    
+                    # Print current and previous closing stock prices with the current time
+                    print(current_time, "Current price:", current_price)
+                    print(current_time, "Previous closing price:", previous_closing_price)
+                
+                # Set the previous closing stock price to the current closing stock price for the next iteration
+                # This allows for the previous closing price to update evry 24 hours   
+                previous_closing_price = current_closing_price
+                
+                # Wait for 4 seconds before checking the stock price again    
+                time.sleep(3)
+            
+            # What to do if not within trading hours        
+            else:
+                # If not within trading hours specified, turn the light off
+                bulb.turn_off()
+                
+                # When light turns off, print the time in which turned off to denote trading hours closed and waiting for the trading floor to open back up
+                print(current_time, "Trading hours closed, waiting for opening...")
+
+                # Printing current time to checking if trading hours have resumed
+                # The printing lets us see in terminal the code is still running
+                while True:
+                    current_time = datetime.datetime.now(pytz.timezone(TIMEZONE))
+                    print(current_time, "Checking if trading hours have resumed...")
+    
+                    # If the current time is within trading hours, it will print that trading hours have resumed
+                    # and apply the break command to tell this second loop to end and resume looping through
+                    # checking stock prices from the current loop 
+                    if current_time.weekday() < 5 and datetime.time(8, 30) <= current_time.time() <= datetime.time(18, 30):
+                        print(current_time, "Trading hours resumed.")
+                        break
+                    
+                    #time.sleep uses seconds, so 2 checks every 2 seconds, 60 every 1 minute, 1800 every 30 minutes, 3600 every 1 hour                    
+                    time.sleep(1800)
+ 
+ #Except command connects to Try command to tell code to print that it encountered error, then restart the code from the Try-line after 30 seconds.                 
+    except Exception as e:
+        print("Encountered error:", e)
+        time.sleep(30)
+        bulb.set_rgb(0, 0, 255)
+        time.sleep(30)
